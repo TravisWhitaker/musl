28e7dd44a43387dc4e2e47285e6d87277d85f251
Travis Whitaker <pi.boy.travis@gmail.com>
2025-04-02 15:43:28 -0700

    Don't hide __init_libc, __libc_start_init

--- src/internal/libc.h
+++ src/internal/libc.h
@@ -37,10 +37,10 @@ struct __libc {
 extern hidden struct __libc __libc;
 #define libc __libc
 
-hidden void __init_libc(char **, char *);
+void __init_libc(char **, char *);
 hidden void __init_tls(size_t *);
 hidden void __init_ssp(void *);
-hidden void __libc_start_init(void);
+void __libc_start_init(void);
 hidden void __funcs_on_exit(void);
 hidden void __funcs_on_quick_exit(void);
 hidden void __libc_exit_fini(void);
