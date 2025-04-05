57fe4b20505d2c3587ead44b6e2dd6d108c0eeaa
Travis Whitaker <pi.boy.travis@gmail.com>
2025-04-05 10:59:06 -0700

    break out __pthread_self

--- src/internal/pthread_impl.c
+++ src/internal/pthread_impl.c
@@ -2,5 +2,12 @@
 
 pthread_t __pthread_self(void)
 {
-  return NULL;
+    uintptr_t tp = __get_tp();
+    uintptr_t pt =
+#ifdef TLS_ABOVE_TP
+        tp - sizeof(struct __pthread) - TP_OFFSET;
+#else
+        tp;
+#endif
+    return (pthread_t)pt;
 }
